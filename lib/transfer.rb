5a252f8856dce812200c0210116cad331fac42f5
David Ritchey <ritchey0713@gmail.com>
2018-04-07 20:27:35 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,17 @@
+require 'pry'
 class Transfer
-  # your code here
+  attr_accessor :receiver, :amount,:sender, :status
+  
+  def initialize(sender, receiver, amount)
+    @receiver = receiver 
+    @sender = sender 
+    @amount = amount
+    @status = "pending"
+  end
+  
+  def valid?
+  end
+  
+  
+  
 end
